c03e9f6f8d11dfa29c77cc9ff727c52d8a1b06fc
Ahmet Cavus <cavus.ahmet@outlook.com>
2020-02-20 15:29:24 +0100

    Adding appcenter packages

--- SandboxApp/App.xaml.cs
+++ SandboxApp/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,6 +12,11 @@ namespace SandboxApp
         public App()
         {
             InitializeComponent();
+
+            AppCenter.Start("android=6aa10396-157b-43e2-883d-19241d2bd259;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
 
             MainPage = new MainPage();
         }
